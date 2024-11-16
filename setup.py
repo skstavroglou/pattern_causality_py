2e48ff68ed82b737805859f68d044f0c017bf12b
wanghui5801 <huiw1128@gmail.com>
2024-11-17 07:41:18 +1100

    change the sign

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ extensions = [
 
 setup(
     name='pattern_causality',
-    version='0.0.1',
+    version='0.0.2',
     description='Pattern Causality Algorithm in Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
